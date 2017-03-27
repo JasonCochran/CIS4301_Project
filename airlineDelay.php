7840cb235d9ba3300489449c3ed25b26574ee9eb
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-03-26 20:21:54 -0400

    Updated main webpage files.

--- airlineDelay.php
+++ airlineDelay.php
@@ -1,3 +1,5 @@
+#!/usr/local/bin/php
+
 <!doctype html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
