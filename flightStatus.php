efd77799137483e6954c3ed20dff76ad4b564b01
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-03 09:45:48 -0400

    Updated HTML formatting to add bounding boxes with white background

--- flightStatus.php
+++ flightStatus.php
@@ -14,38 +14,42 @@
     <!-- Example row of columns -->
     <div class="row">
         <div class="col-md-4">
-            <!-- Search side bar -->
-            <h3>Search Settings</h3>
-            <form>
-                <div class="form-group">
-                    <label for="carrierInput">Carrier:</label>
-                    <select class="form-control" id="carrierInput">
-                        <option>JetBlue</option>
-                    </select>
-                    <br>
-                    <label for="carrierInput">Departure Date:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Month" id="month"/>
-                        <span class="input-group-addon">-</span>
-                        <input type="text" class="form-control" placeholder="Day" id="day"/>
-                        <span class="input-group-addon">-</span>
-                        <input type="text" class="form-control" placeholder="Year" id="year"/>
-                    </div>
-                    <br>
-                    <label for="carrierInput">Airport Departure and Arrival:</label>
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Departure Airport" id="departure"/>
-                        <span class="input-group-addon">-</span>
-                        <input type="text" class="form-control" placeholder="Arrival Airport" id="airport"/>
-                    </div>
-                    <br>
-                    <label>
-                        <input type="checkbox" id="weatherBool"> Account for inclement weather
-                    </label>
-                    <br>
 
+            <div class="panel panel-default">
+                <div class="panel-heading">Search Settings</div>
+                <div class="panel-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="carrierInput">Carrier:</label>
+                            <select class="form-control" id="carrierInput">
+                                <option>JetBlue</option>
+                            </select>
+                            <br>
+                            <label for="carrierInput">Departure Date:</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Month" id="month"/>
+                                <span class="input-group-addon">-</span>
+                                <input type="text" class="form-control" placeholder="Day" id="day"/>
+                                <span class="input-group-addon">-</span>
+                                <input type="text" class="form-control" placeholder="Year" id="year"/>
+                            </div>
+                            <br>
+                            <label for="carrierInput">Airport Departure and Arrival:</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Departure Airport" id="departure"/>
+                                <span class="input-group-addon">-</span>
+                                <input type="text" class="form-control" placeholder="Arrival Airport" id="airport"/>
+                            </div>
+                            <br>
+                            <label>
+                                <input type="checkbox" id="weatherBool"> Account for inclement weather
+                            </label>
+                            <br>
+
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
         <div class="col-md-8">
             <h2> There is a ___% chance your flight will be delayed. </h2>
