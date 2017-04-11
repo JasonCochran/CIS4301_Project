e16a989eb25d688c5fc4427f5138b95ca7e3aa82
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-11 00:21:49 -0400

    Change extension of our 404 file

---
+++ 404.php
@@ -0,0 +1,18 @@
+#!/usr/local/bin/php
+
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+
+<?php include("includes/header.php"); ?>
+
+<body>
+<div class="container">
+    <h1>Page Not Found</h1>
+</div>
+</body>
+<?php include("includes/footer.html"); ?>
+
+</html>
