c183faec1be6bde1f119688e5bf9d084ed61e257
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-10 21:59:01 -0400

    More changes to form submission. Started to get data from database.

--- calendar.php
+++ calendar.php
@@ -7,6 +7,48 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
 
 <?php
+
+$connection = oci_connect($username = 'jcochran',
+    $password = '972903TUXy12?',
+    $connection_string = '//oracle.cise.ufl.edu/orcl');
+
+$statement = oci_parse($connection, 'SELECT * FROM airports');
+oci_execute($statement);
+
+// HTML Table declaration
+echo "
+		<table class='table table-hover'>
+		<thead>
+		<tr>
+		<th>IATA</th>
+		<th>Airport Name</th>
+		<th>City</th>
+		<th>State</th>
+		<th>Latitude</th>
+		<th>Longitude</th>
+		</tr>
+		</thead>
+	";
+
+$calendarDelays = array();
+
+// get data from table and format it on the table
+while (($row = oci_fetch_object($statement))) {
+    echo "<tr>";
+    echo "<td>" . $row->IATA . "</td>";
+    echo "<td>" . $row->AIRPORTNAME . "</td>";
+    echo "<td>" . $row->CITY . "</td>";
+    echo "<td>" . $row->STATE . "</td>";
+    echo "<td>" . $row->LATITUDE . "</td>";
+    echo "<td>" . $row->LONGITUDE . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+// close Oracle database connection and free statements
+oci_free_statement($statement);
+oci_close($connection);
+
 ini_set('display_errors', 1);
 $validForm = true;
 $submitted = false;
@@ -46,19 +88,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $monthErr = "Only alphanumeric characters accepted";
     }
 }
-
-
 ?>
+
 <body>
 
 <div class="container">
     <div class="row">
         <div class="col-md-4">
             <div class="panel panel-default">
-                <div class="panel-heading">Calendar</div>
                 <div class="panel-body">
-                    <p>View delay information as a heatmap. Specify constraints below to further
-                        refine the view displayed. </p>
+                    <p>Input a month to view how delay times change over the month.  </p>
                     <form>
                         <div class="form-group">
                             <input type="month" name="filter[]" id="filter" value="Month" onclick="displayCheck(this);">
@@ -95,7 +134,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </div>
         <div class="col-md-8">
-            <h1 class="text-center">Calendar Placeholder</h1>
+            <h1 class="text-center">Monthly Heatmap of Flight Delays</h1>
             <table class="table table-bordered">
                 <tr>
                     <th>Sat.</th>
