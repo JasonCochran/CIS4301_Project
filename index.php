afa6e2822cec6250e0ab25dc125c523ed940b8fc
Jonathan Osorio <josoriodev@gmail.com>
2017-04-12 00:46:32 -0400

    New design
    Also cleaned up a few other things

--- index.php
+++ index.php
@@ -1,39 +1,26 @@
 #!/usr/local/bin/php
 
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<!DOCTYPE HTML>
+<html lang="en">
 
 <?php include("includes/header.php"); ?>
+<?php include("includes/navbar.html"); ?>
 
+<head>
+	<title>JetBlue Delay Predictor</title>
+	<!-- Color Chrome Nav Bar -->
+	<meta name="theme-color" content="#3B4163"/>
+</head>
 <body>
+	<header>
+		<h1 id="header-title">JetBlue Delay Predictor</h1>
+		<h2 id="header-subtitle">Never be late to your next destination.</h2>
+	</header>
 
-<section class="banner" id="banner">
-    <div class="row">
-        <div class="col-lg-12">
-            <img src="assets/index_header2.jpg" width="100%">
-        </div>
-    </div>
-</section>
-
-<div class="container">
-    <!-- Example row of columns -->
-    <div class="row">
-        <div class="col-md-12">
-            <h2>Welcome to our Flight Delay Predictor Application!</h2>
-            <p>You can utilize this website to predict the probability of your flight being delayed.
-            This can be particularly useful if you travel during the winter months or times with a
-            lot of travel.</p>
-        </div>
-    </div>
-
-    <hr>
-
-</div> <!-- /container -->
-
-<?php include("includes/footer.html"); ?>
-
+	<div id="home-page-content">
+		<div class="p-container">
+			<?php include("includes/footer.html"); ?>
+		</div>
+	</div>
 </body>
 </html>
