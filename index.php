8d04398da4846268d74094b0c41c549cf191b873
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-12 18:54:08 -0400

    Modified the text content of index.php

--- index.php
+++ index.php
@@ -27,17 +27,19 @@
 			<div class="col-lg-4">
 				<img src="http://www.cise.ufl.edu/~josorio/flight/assets/stats.svg" alt="Stats image" width="140" height="140">
 				<h2>Dataset</h2>
-				<p>Using flight data provided by the Bureau of Transportation Statistics, our predictor can compare your next flight against previous records to give you an estimate on how likely your flight is to be delayed.</p>
+				<p>Using flight data provided by the Bureau of Transportation Statistics, our predictor can determine how likely it is that your flight
+                    will be delayed using historical data.</p>
 			</div><!-- /.col-lg-4 -->
 			<div class="col-lg-4">
 				<img src="http://www.cise.ufl.edu/~josorio/flight/assets/database.svg" alt="Database image" width="140" height="140">
 				<h2>Database</h2>
-				<p>Through the Oracle Database, our predictor can quickly lookup and retrieve the information requested, letting you not only save time on your next flight, but also while you're here.</p>
+				<p>Through the Oracle Database, our predictor can quickly lookup and retrieve the information requested,
+                    letting you not only save time and the headache of directly interacting with queries.</p>
 			</div><!-- /.col-lg-4 -->
 			<div class="col-lg-4">
 				<img src="http://www.cise.ufl.edu/~josorio/flight/assets/programmer.svg" alt="Programming" width="140" height="140">
 				<h2>Programming</h2>
-				<p>Built with the best web programming tools available, including PHP, HTML5, CSS3, Bootstrap, and jQuery.</p>
+				<p>Built with the best web programming and markup tools available; including PHP, HTML5, CSS3, Bootstrap, and jQuery.</p>
 			</div><!-- /.col-lg-4 -->
 			</div><!-- /.row -->
 
@@ -46,7 +48,7 @@
 
 			<div class="row featurette">
 			<div class="col-md-7">
-				<h2 class="featurette-heading">Flight Delay Predictor.<span class="text-muted"> It'll blow your mind.</span></h2>
+				<h2 class="featurette-heading">Predict the future.<span class="text-muted"> Of your flight delay...</span></h2>
 				<p class="lead">Simply input some details about your next flight with JetBlue and get an instant delay prediction.</p>
 			</div>
 			<div class="col-md-5">
@@ -59,8 +61,8 @@
 
 			<div class="row featurette">
 			<div class="col-md-7 col-md-push-5">
-				<h2 class="featurette-heading">Calendar of Delays.<span class="text-muted"> Skip that delay!.</span></h2>
-				<p class="lead">See all the possible delays of the month with this tool. It's that easy.</p>
+				<h2 class="featurette-heading">Delay Calendar.<span class="text-muted"> Not anymore!</span></h2>
+				<p class="lead">See how the delays change over a month. Now you know what days are bad for travel.</p>
 			</div>
 			<div class="col-md-5 col-md-pull-7">
 				<img class="featurette-image img-responsive center-block" src="http://www.cise.ufl.edu/~josorio/flight/assets/weekly-calendar.svg" alt="Weekly calendar image" width="250" height="250">
@@ -71,8 +73,8 @@
 
 			<div class="row featurette">
 			<div class="col-md-7">
-				<h2 class="featurette-heading">See the stats for yourself.<span class="text-muted"> For those that like data.</span></h2>
-				<p class="lead">With our data browser, see the data we're using yourself on your browser!</p>
+				<h2 class="featurette-heading">Stats.<span class="text-muted"> For those that like data.</span></h2>
+				<p class="lead">With our data browser, you can see all the tuples we have stored in the database at any given time!</p>
 			</div>
 			<div class="col-md-5">
 				<img class="featurette-image img-responsive center-block" src="http://www.cise.ufl.edu/~josorio/flight/assets/browser.svg" alt="Browser image" width="250" height="250">
