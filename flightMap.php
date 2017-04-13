6710cccd4bbe78f0c67161d0f744e13a66f437e7
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-13 13:37:45 -0400

    Finished up the flight map functionality

--- flightMap.php
+++ flightMap.php
@@ -19,81 +19,41 @@
         $password = $GLOBALS['password'],
         $connection_string = '//oracle.cise.ufl.edu/orcl');
 
-    // write your SQL query here (you may use parameters from $_GET or $_POST if you need them)
-    $query = oci_parse($connection, "SELECT * FROM AIRPORTS" );
-
-    $table = array();
-    $table['cols'] = array(
-        /* define your DataTable columns here
-        * each column gets its own array
-        * syntax of the arrays is:
-        * label => column label
-        * type => data type of column (string, number, date, datetime, boolean)
-        */
-        array('label' => 'Label of column 1', 'type' => 'string'),
-        array('label' => 'Label of column 2', 'type' => 'number'),
-        array('label' => 'Label of column 3', 'type' => 'number')
-        // etc...
-    );
+    $query = oci_parse($connection, "SELECT STATE, COUNT(*) AS AIRPORTSTOTAL FROM AIRPORTS GROUP BY STATE");
+    oci_execute($query);
 
     $rows = array();
-    while($r = oci_fetch_row($query)) {
-        $temp = array();
-        // each column needs to have data inserted via the $temp array
-        $temp[] = array('v' => $r['column1']);
-        $temp[] = array('v' => $r['column2']);
-        $temp[] = array('v' => $r['column3']);
-        // etc...
+    $table = array();
+    $table['cols'] = array( array('label' => 'State', 'type' => 'string'),
+    array('label' => 'Delay %', 'type' => 'number') );
 
-        // insert the temp array into $rows
-        $rows[] = array('c' => $temp);
-    }
+$rows = array();
+while ($r = oci_fetch_array($query, OCI_ASSOC + OCI_RETURN_NULLS)) {
+    $temp = array();
+//The below col names have to be in upper caps.
+    // echo $r["STATE"];
+    $temp[] = array('v' => (string)$r["STATE"]);
 
-    oci_free_statement($query);
-    oci_close($connection);
+$temp[] = array('v' => (int)$r["AIRPORTSTOTAL"]);
+$rows[] = array('c' => $temp);
+}
 
-    // populate the table with rows of data
-    $table['rows'] = $rows;
+$table['rows'] = $rows;
+$jsonTable = json_encode($table);
 
-    // encode the table as JSON
-    $jsonTable = json_encode($table);
-
-    // return the JSON data
-    echo $jsonTable;
     ?>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
         google.charts.load('current', {'packages':['geochart']});
         google.charts.setOnLoadCallback(drawRegionsMap);
 
+
+
         function drawRegionsMap() {
-            var data = google.visualization.arrayToDataTable([
-                ['Country',   'Latitude'],
-                ['Algeria', 36], ['Angola', -8], ['Benin', 6], ['Botswana', -24],
-                ['Burkina Faso', 12], ['Burundi', -3], ['Cameroon', 3],
-                ['Canary Islands', 28], ['Cape Verde', 15],
-                ['Central African Republic', 4], ['Ceuta', 35], ['Chad', 12],
-                ['Comoros', -12], ['Cote d\'Ivoire', 6],
-                ['Democratic Republic of the Congo', -3], ['Djibouti', 12],
-                ['Egypt', 26], ['Equatorial Guinea', 3], ['Eritrea', 15],
-                ['Ethiopia', 9], ['Gabon', 0], ['Gambia', 13], ['Ghana', 5],
-                ['Guinea', 10], ['Guinea-Bissau', 12], ['Kenya', -1],
-                ['Lesotho', -29], ['Liberia', 6], ['Libya', 32], ['Madagascar', null],
-                ['Madeira', 33], ['Malawi', -14], ['Mali', 12], ['Mauritania', 18],
-                ['Mauritius', -20], ['Mayotte', -13], ['Melilla', 35],
-                ['Morocco', 32], ['Mozambique', -25], ['Namibia', -22],
-                ['Niger', 14], ['Nigeria', 8], ['Republic of the Congo', -1],
-                ['Réunion', -21], ['Rwanda', -2], ['Saint Helena', -16],
-                ['São Tomé and Principe', 0], ['Senegal', 15],
-                ['Seychelles', -5], ['Sierra Leone', 8], ['Somalia', 2],
-                ['Sudan', 15], ['South Africa', -30], ['South Sudan', 5],
-                ['Swaziland', -26], ['Tanzania', -6], ['Togo', 6], ['Tunisia', 34],
-                ['Uganda', 1], ['Western Sahara', 25], ['Zambia', -15],
-                ['Zimbabwe', -18]
-            ]);
+            var data = new google.visualization.DataTable(<?=$jsonTable?>);
 
             var options = {
-                region: 'US', // USA
+                region: 'US',
                 resolution: 'provinces',
                 colorAxis: {colors: ['#C7FFAD', '#015FB2']},
                 backgroundColor: 'white',
@@ -114,7 +74,8 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Map of Delays</div>
                 <div class="panel-body">
-                    View all of the delays in a heatmap across the United States.
+                    View all of the delays in a heatmap across the United States. This table allows you to visualize
+                    how delays change across the country. Additionally,
                 </div>
             </div>
         </div>
