dadf786e4bbb6b72c1aecd3d4be0e93fb37de493
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-13 11:23:07 -0400

    Updated flight predictor with query

--- flightPredictor.php
+++ flightPredictor.php
@@ -1,214 +1,195 @@
 #!/usr/local/bin/php
 
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<html>
+<head>
+    <title>JetBlue Delay Predictor Calendar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- Raleway & Roboto -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+</head>
+
+</html>
 
 <?php
-// TODO remove this
+include("includes/solid_navbar.html");
+include('globalVariables.php');
 ini_set('display_errors', 1);
-$validForm = true;
-$submitted = false;
-$input = false;
-$numFilters = 0;
+$validForm = true;			// form is valid unless an invalid value is input
+$submitted = false;			// if the whole form is valid, then this will become true and form can be submitted
+$input = false;				// to prevent an empty form from running a blank query
 ?>
 
+<!-- db stuff -->
 <?php
-function runQuery($departureAirportClean, $arrivalAirportClean, $dayClean, $monthClean, $yearClean)
-{
-    include('globalVariables.php');
+function runQuery($originClean, $destClean, $dayClean, $monthClean) {
+    $connection = oci_connect($username = $GLOBALS['username'], $password = $GLOBALS['password'], $connection_string = '//oracle.cise.ufl.edu/orcl');
 
-    $connection = oci_connect($username = $GLOBALS['username'],
-        $password = $GLOBALS['password'],
-        $connection_string = '//oracle.cise.ufl.edu/orcl');
+    $date = $dayClean."-".$monthClean."-"."16";
+/*
+    $alterSession = "ALTER SESSION SET NLS_DATE_FORMAT = DD-MM-YYYY";
+    $query = oci_parse($connection,$alterSession );
+    oci_execute($query);
+ */
 
-    // $departureAirportClean = "JFK";
-    // $arrivalAirportClean = "MCO";
-    // $dayClean = "2016-12-21";
-    $dateClean = $yearClean . "-" . $monthClean . "-" . $dayClean;
-
-    echo $yearClean, $monthClean, $dayClean;
-
-// TODO write the query for this
-    $statement = oci_parse($connection, "SELECT * FROM FLIGHTS, CANCELLATIONS
+    $monsterQuery = "SELECT FLIGHTS.FLIGHTDATE, FLIGHTS.FLIGHTNUMBER FROM FLIGHTS, CANCELLATIONS
  WHERE FLIGHTS.FLIGHTNUMBER = CANCELLATIONS.FLIGHTNUMBER
    AND FLIGHTS.FLIGHTDATE = CANCELLATIONS.FLIGHTDATE
    AND FLIGHTS.ORIGINAIRPORT =:departureAirport_bv
    AND FLIGHTS.DESTINATIONAIRPORT =:arrivalAirport_bv 
-   AND FLIGHTS.FLIGHTDATE =:date_bv");
+   AND FLIGHTS.FLIGHTDATE =:date_bv";
 
-    $statement_2 = oci_parse($connection, "SELECT * FROM 
+    $monsterQuery_2 = "SELECT * FROM
 FLIGHTS WHERE FLIGHTS.ORIGINAIRPORT=:departureAirport_bv
-   AND FLIGHTS.DESTINATIONAIRPORT=:arrivalAirport_bv 
-   AND FLIGHTS.FLIGHTDATE =:date_bv");
+ AND FLIGHTS.DESTINATIONAIRPORT=:arrivalAirport_bv 
+ AND FLIGHTS.FLIGHTDATE =:date_bv";
 
-// AND FLIGHTS.FLIGHTDATE = 'YEAR-MONTH_DAY'
-// TODO need to actually define the $dateClean variable
-    oci_bind_by_name($statement, ":date_bv", $dateClean);
-    oci_bind_by_name($statement, ":departureAirport_bv", $departureAirportClean);
-    oci_bind_by_name($statement, ":arrivalAirport_bv", $arrivalAirportClean);
+
+    $statement = oci_parse($connection, $monsterQuery);
+    $statement_2 = oci_parse($connection, $monsterQuery_2);
+
+    oci_bind_by_name($statement, ":departureAirport_bv",$destClean );
+    oci_bind_by_name($statement, ":arrivalAirport_bv", $originClean);
+    oci_bind_by_name($statement, ":date_bv", $date);
     oci_execute($statement);
-
-    oci_bind_by_name($statement_2, ":date_bv", $dateClean);
-    oci_bind_by_name($statement_2, ":departureAirport_bv", $departureAirportClean);
-    oci_bind_by_name($statement_2, ":arrivalAirport_bv", $arrivalAirportClean);
+/*
+    oci_bind_by_name($statement_2, ":departureAirport_bv",$destClean );
+    oci_bind_by_name($statement_2, ":arrivalAirport_bv", $originClean);
+    oci_bind_by_name($statement_2, ":day_bv", $dayClean);
+    oci_bind_by_name($statement_2, ":month_bv", $monthClean);
     oci_execute($statement_2);
 
-    $delays = oci_fetch_array($statement);
-    $flightDelayCount = count($delays);
+    $allFlights = oci_fetch_object($statement_2);
+    $allFlightsCount = count($allFlights);
 
-    $flights = oci_fetch_array($statement_2);
-    $flightCount = count($flights, COUNT_NORMAL);
+    echo $allFlightsCount;
+*/
+    // output result as a table
 
-    echo $flightDelayCount;
-    echo $flightCount;
+    $count = 0;
+    while (($row = oci_fetch_object($statement))) {
+        $count++;
+    }
 
-    $prediction = 100 * $flightDelayCount / $flightCount;
 
-    echo $prediction;
+    echo $count;
 
+    // oci_free_statement($query);
     oci_free_statement($statement);
-    oci_free_statement($statement_2);
+  //  oci_free_statement($statement_2);
     oci_close($connection);
 }
+?>
 
-?>
+<!-- form validation -->
 <?php
- include("includes/header.php");
-include("includes/solid_navbar.html");
-?>
-<?php
-function test_input($data)
-{
+// helps prevent malicious HTML input
+function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
 
-$monthClean = $dayClean = $yearClean = $arrivalAirportClean = $departureAirportClean = "";
+// input variables that will be sanitized
+$originClean = $destClean = "";
 
+// validates each variable
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $monthClean = test_input($_POST["month-filter"]);
-    if (!is_numeric($monthClean) && !empty($monthClean)) {
+    // DONE - origin
+    $originClean = test_input($_POST["origin-filter"]);
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $originClean)) {
         $validForm = false;
-        $monthErr = "Only numeric characters accepted";
+        $orgErr = "Only alphanumeric characters accepted";
+    }
+
+    // DONE - destination
+    $destClean = test_input($_POST["destination-filter"]);
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $destClean)) {
+        $validForm = false;
+        $destErr = "Only alphanumeric characters accepted";
     }
 
     $dayClean = test_input($_POST["day-filter"]);
-    if (!is_numeric($dayClean) && !empty($dayClean)) {
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $dayClean)) {
         $validForm = false;
-        $dayErr = "Only numeric characters accepted";
+        $dayErr = "Only alphanumeric characters accepted";
     }
 
-    $yearClean = test_input($_POST["year-filter"]);
-    if (!is_numeric($yearClean) && !empty($yearClean)) {
+    $monthClean = test_input($_POST["month-filter"]);
+    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $monthClean)) {
         $validForm = false;
-        $yearErr = "Only numeric characters accepted";
+        $monthErr = "Only alphanumeric characters accepted";
+    }
+}
+?>
+<div class="container">
+    <form method="post" action="">
+        <!-- Origin -->
+        <input type="checkbox" name="filter[]" id="filter" value="Origin" onclick="displayCheck(this);"> Origin
+        <input type="text" id="Origin" name="origin-filter" placeholder='JFK'>
+        <span class="error"><?php echo $orgErr; ?></span>
+
+        <!-- Destination -->
+        <input type="checkbox" name="filter[]" id="filter" value="Destination" onclick="displayCheck(this);"> Destination
+        <input type="text" id="Destination" name="destination-filter" placeholder="JAX">
+        <span class="error"><?php echo $destErr; ?></span>
+
+        <!-- Day -->
+        <input type="checkbox" name="filter[]" id="filter" value="Destination" onclick="displayCheck(this);"> Day
+        <input type="text" id="Day" name="day-filter" placeholder="JAX">
+        <span class="error"><?php echo $dayErr; ?></span>
+
+        <!-- Month -->
+        <input type="checkbox" name="filter[]" id="filter" value="Month" onclick="displayCheck(this);"> Month
+        <input type="text" id="Month" name="month-filter" placeholder="JAX">
+        <span class="error"><?php echo $monthErr; ?></span>
+
+        <input type="submit" class="btn" name="submit" value="Submit">
+    </form>
+</div>
+
+<!-- After form has been submitted -->
+<?php
+if(isset($_POST['submit'])) {
+
+    $stack = array();
+    array_push($stack, $originClean);
+    array_push($stack, $destClean);
+    array_push($stack, $dayClean);
+    array_push($stack, $monthClean);
+
+    foreach ($stack as $value) {
+        // if there's at least one input, we can submit the query
+        if(!empty($value)) {
+            $input = true;
+        }
     }
 
-    $departureAirportClean = test_input($_POST["departureAirport-filter"]);
-    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $departureAirportClean)) {
-        $validForm = false;
-        $departureAirportErr = "Only alphanumeric characters accepted";
-    }
-
-    $arrivalAirportClean = test_input($_POST["arrivalAirport-filter"]);
-    if (!preg_match("/^[a-zA-Z0-9 ]*$/", $arrivalAirportClean)) {
-        $validForm = false;
-        $arrivalAirportErr = "Only alphanumeric characters accepted";
+    // if the form is valid, then it can be submitted
+    if($validForm) {
+        $submitted = true;
     }
 }
 
+// if form is valid and submit has been clicked, run the query
+if ($validForm && $submitted && $input) {
+    runQuery($originClean, $destClean, $dayClean, $monthClean);
+}
+
+// reset form
+$validForm = true;
+$submitted = false;
+$input = false;
 ?>
 
-<body>
-
-<div class="container">
-
-            <div class="panel panel-default">
-                <div class="panel-heading">Search Settings</div>
-                <div class="panel-body">
-                    <form method="post" action="">
-                        <div class="form-group">
-                            <br>
-                            <label for="carrierInput">Departure Date:</label>
-                            <div class="input-group">
-
-                                <input type="text" class="form-control" name="filter[]" id="filter" value="Month"
-                                       onclick="displayCheck(this);">
-                                <input type="text" id="Month" name="Month-filter" style="display:none">
-                                <span class="error"><?php echo $monthErr; ?></span>
-
-                                <span class="input-group-addon">-</span>
-
-                                <input type="text" class="form-control" name="filter[]" id="filter" value="Day"
-                                       onclick="displayCheck(this);">
-                                <input type="text" id="Day" name="Day-filter" style="display:none">
-                                <span class="error"><?php echo $dayErr; ?></span>
-
-                                <span class="input-group-addon">-</span>
-
-                                <input type="text" class="form-control" name="filter[]" id="filter" value="Year"
-                                       onclick="displayCheck(this);">
-                                <input type="text" id="Year" name="Year-filter" style="display:none">
-                                <span class="error"><?php echo $yearErr; ?></span>
-                            </div>
-                            <br>
-                            <label for="carrierInput">Airport Departure and Arrival:</label>
-                            <div class="input-group">
-
-                                <input type="text" name="filter[]" id="filter"
-                                       value="Departure Airport" onclick="displayCheck(this);">
-                                <input type="text" id="Departure Airport" name="DepartureAirport-filter"
-                                       style="display:none">
-                                <span class="error"><?php echo $departureAirportErr; ?></span>
-
-                                <span class="input-group-addon">-</span>
-
-                                <input type="text"= name="filter[]" id="filter"
-                                       value="Arrival Airport" onclick="displayCheck(this);">
-                                <input type="text" id="Arrival Airport" name="ArrivalAirport-filter"
-                                       style="display:none">
-                                <span class="error"><?php echo $arrivalAirportErr; ?></span>
-
-                            </div>
-                            <br>
-                            <input type="submit" class="btn" name="submit" value="Submit">
-                        </div>
-                    </form>
-                    <?php
-                    if (isset($_POST['submit'])) {
-                        $stack = array();
-                        array_push($stack, $departureAirportClean);
-                        array_push($stack, $arrivalAirportClean);
-                        array_push($stack, $dayClean);
-                        array_push($stack, $monthClean);
-                        array_push($stack, $yearClean);
-
-                        if ($validForm) {
-                            $submitted = true;
-                        }
-                    }
-
-                    if ($validForm && $submitted) {
-                        runQuery($departureAirportClean, $arrivalAirportClean, $dayClean, $monthClean, $yearClean);
-                    }
-
-                    $validForm = true;
-                    $submitted = false;
-                    $input = false;
-                    ?>
-                </div>
-    </div>
-
-</div> <!-- /container -->
-<div class="container">
-    <hr>
-    <?php include("includes/footer.html"); ?>
-</div>
-</body>
-</html>
+<?php
+//
+//include("includes/footer.html");
+?>
